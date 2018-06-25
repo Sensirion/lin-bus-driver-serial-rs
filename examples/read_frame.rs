a2c15ee4131b40999f8c19a829cf0d33757fecf2
Raphael Nestler <raphael.nestler@sensirion.com>
2018-06-25 18:00:52 +0200

    Add usage example

---
+++ examples/read_frame.rs
@@ -0,0 +1,18 @@
+extern crate lin_bus_driver_serial;
+
+use lin_bus_driver_serial::SerialLin;
+use lin_bus_driver_serial::serial as serial;
+use lin_bus_driver_serial::lin_bus::{Master, PID};
+
+fn main() {
+    let pid = PID::from_id(42);
+
+    let mut lin = SerialLin(serial::open("/dev/ttyS0").expect("Opening serial port failed"));
+
+    lin.send_wakeup().expect("Sending wakeup failed");
+
+    let frame = lin.read_frame(pid, 4).expect("Reading frame failed");
+
+    let data = frame.get_data();
+    println!("Data: {:?}", data);
+}
