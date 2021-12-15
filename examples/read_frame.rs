b09aeddb33b4b5f601806a0de364dc7ed48630a7
Raphael Nestler <raphael.nestler@sensirion.com>
2021-12-15 16:17:06 +0100

    Adapt example to use const PID

--- examples/read_frame.rs
+++ examples/read_frame.rs
@@ -5,13 +5,13 @@ use lin_bus_driver_serial::serial;
 use lin_bus_driver_serial::SerialLin;
 
 fn main() {
-    let pid = PID::from_id(42);
+    const TEST_PID: PID = PID::from_id(42);
 
     let mut lin = SerialLin(serial::open("/dev/ttyS0").expect("Opening serial port failed"));
 
     lin.send_wakeup().expect("Sending wakeup failed");
 
-    let frame = lin.read_frame(pid, 4).expect("Reading frame failed");
+    let frame = lin.read_frame(TEST_PID, 4).expect("Reading frame failed");
 
     let data = frame.get_data();
     println!("Data: {:?}", data);
